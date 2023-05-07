0bf9245bec7e39fcf5f03ada912fe82da86af3b6
Peter Oti <71887594+PetxCode@users.noreply.github.com>
2023-05-07 22:25:43 +0100

    start point

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import styled from 'styled-components'
+
+const App = () => {
+  return (
+    <Container>
+      App
+    </Container>
+  )
+}
+
+export default App
+
+const Container = styled.div``
